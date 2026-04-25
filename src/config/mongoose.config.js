502157cf65387808f9a9a8b419bd4bafc639ef70
gtshilpa123 <127193630+gtshilpa123@users.noreply.github.com>
2026-04-25 18:40:34 +0530

    Add files via upload

---
+++ src/config/mongoose.config.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { categorySchema } from "../features/product/category.schema.js";
+dotenv.config();
+const url = process.env.DB_URL;
+
+export const connectUsingMongoose = () => {
+  try {
+    mongoose
+      .connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => {
+        console.log("Mongodb using mongoose is connected");
+        addCategories();
+      })
+      .catch((err) => console.log(err));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function addCategories() {
+  const CategoryModel = mongoose.model("Category", categorySchema);
+  const categories = await CategoryModel.find();
+  if (!categories || categories.length == 0) {
+    await CategoryModel.insertMany([
+      { name: "Books" },
+      { name: "Clothing" },
+      { name: "Electronics" },
+    ]);
+  }
+  console.log("Categories are added");
+}
+
+// categories are added when we start our application and database is connected.
