bfaef09255af9b4f4cf04f2742011e25b0140402
gtshilpa123 <127193630+gtshilpa123@users.noreply.github.com>
2026-04-25 18:22:26 +0530

    Add files via upload

---
+++ src/features/cartitems/cartitems.routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { CartItemsController } from "./cartitems.controller.js";
+
+const cartRouter = express.Router();
+const cartController = new CartItemsController();
+
+cartRouter.post("/", (req, res, next) => cartController.add(req, res, next));
+
+cartRouter.get("/", (req, res, next) => cartController.get(req, res, next));
+
+cartRouter.delete("/:id", (req, res, next) =>
+  cartController.delete(req, res, next)
+);
+
+export default cartRouter;
