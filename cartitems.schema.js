97d7ccb7d1e885ad029c2d407770c484c3ebd799
gtshilpa123 <127193630+gtshilpa123@users.noreply.github.com>
2026-04-25 18:25:10 +0530

    Delete cartitems.schema.js

--- cartitems.schema.js
+++
@@ -1,7 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-export const cartSchema = new Schema({
-  productID: { type: mongoose.Schema.Types.ObjectId, ref: "Product" }, // product is collection from which productId is coming
-  userID: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  quantity: Number,
-});
